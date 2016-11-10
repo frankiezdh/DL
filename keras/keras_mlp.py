b6f91e8987185beb1579e841e4a09387412cf912
frankiezdh <frankiezdh@gmail.com>
2016-11-10 22:36:44 +0800

    重构，增加对数据进行预测的功能，以及打印图像

--- keras/keras_mlp.py
+++ keras/keras_mlp.py
@@ -1,10 +1,12 @@
 
-import numpy as np
-np.random.seed(1331)
-
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Activation
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(1331)
+
 
 def f(x):
     return x * 0.5 + 2
@@ -20,7 +22,7 @@ def random_down(_x):
     return [x, f(x) - 10 * np.random.random_sample()]
 
 
-SIZE = 10000
+SIZE = 1000
 
 X = np.random.uniform(-100, 100, SIZE).reshape(SIZE, 1)
 X1 = np.asarray([random_up(k) for k in X])
@@ -47,4 +49,29 @@ model.compile(optimizer='sgd', loss='mse', metrics=['accuracy'])
 print(model.summary())
 
 model.fit(X_train, Y_train, verbose=1, nb_epoch=200, batch_size=128)
+
+x = np.random.uniform(-200, 200, SIZE).reshape(SIZE, 1)
+x1 = np.asarray([random_up(k) for k in X])
+x2 = np.asarray([random_down(k) for k in X])
+X_pred = np.concatenate((X1, X2))
+Y_pred = model.predict(X_pred)
+
+red = []
+blue = []
+
+for i in range(0, len(X_pred)):
+    if (Y_pred[i][0] <= Y_pred[i][1]):
+        red.append([X_pred[i][0], X_pred[i][1]])
+    else:
+        blue.append([X_pred[i][0], X_pred[i][1]])
+
+
+plt.scatter(np.asarray(red)[:, 0], np.asarray(red)[:, 1], c='red')
+plt.scatter(np.asarray(blue)[:, 0], np.asarray(blue)[:, 1], c='blue')
+plt.plot(np.linspace(-200, 200, SIZE), f(np.linspace(-200, 200, SIZE)))
+
+plt.show()
+
+
+
 
